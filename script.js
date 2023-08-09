c29cc0f76500f2c49902d093d59b70f68f24c595
muazzamgujjar <muazzamgujjarmuazzam@gmail.com>
2023-08-09 00:03:41 -0700

    third coment

---
+++ script.js
@@ -0,0 +1,11 @@
+let t= document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.row)
+console.log(t.head)
+console.log(t.foot)
+console.log(t.body)
+console.log(t.cells)
+
+
+
+
